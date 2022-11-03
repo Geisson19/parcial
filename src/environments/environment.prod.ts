f2a4b903bf0f21dfb25ea2eaf71a28872c256449
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-11-03 18:24:24 -0500

    module

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+const baseUrl = 'https://localhost:3000';
 export const environment = {
-  production: true
+  production: true,
+  baseUrl
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const baseUrl = 'http://localhost:3000';
 export const environment = {
-  production: false
+  production: false,
+  baseUrl
 };
 
 /*
@@ -13,4 +15,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
