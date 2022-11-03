80eae1bb22443a8ce96530ceaab94b7673f17bf9
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-11-03 18:36:23 -0500

    service http

---
+++ src/app/cafe/cafe.ts
@@ -0,0 +1,11 @@
+export class Cafe {
+  constructor(
+        public id: number,
+        public nombre: string,
+        public tipo: string,
+        public region: number,
+        public sabor: string,
+        public altura: string,
+        public imagen: string,
+    ) { }
+}
